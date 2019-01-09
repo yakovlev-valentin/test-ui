2b8710e7258e657bf224e19165041e8e09e82159
Valentin Yakovlev <vyakovlev.codica@gmail.com>
2019-01-09 14:07:36 +0200

    eport plugins

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import UI from './plugins'
+import VuiCodica from './plugins'
 import '@mdi/font/scss/materialdesignicons.scss'
 import '@/styles/index.scss'
-Vue.use(UI)
+Vue.use(VuiCodica)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+export default VuiCodica
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -19,13 +19,5 @@ const VuiCodica = {
 	}
 }
 
-// const install = (Vue) => {
-// 	components.forEach(component => {
-// 		Vue.component(component.name, component)
-// 	})
-// 	if (typeof window !== 'undefined' && window.Vue) {
-// 		install(window.Vue);
-// 	}
-// }
 use(VuiCodica)
 export default VuiCodica
